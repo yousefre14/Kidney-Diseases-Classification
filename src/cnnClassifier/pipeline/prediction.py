b3ddf96f97b321d32919b5df8aea6a3008c5184a
yousefre14 <yousef.jo.reda14@gmail.com>
2025-09-28 17:54:25 +0300

    prediction added

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -1,25 +1,30 @@
-import os
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import os
 
 
-class predictionPipeline:
-    def_init_(self,filename):
-        self.filename=filename
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
     
-
-
-
     def predict(self):
-        model = load_model(os.path.join("artifacts","training","model.h5"))
-        
-        imagename= self.filename
-        test_image= image.load_img(imagename, target_size =(224,224))
-        test_image=image.img_to_array(test_image)
-        test_image= np.expand_dims(test_image,axis=0)
-        result = np.argmax(model.predict(test_image),axis=1)
+        # load model
+        model = load_model(os.path.join("model", "model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
         print(result)
 
-        if
-
+        if result[0] == 1:
+            prediction = 'Tumor'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Normal'
+            return [{"image": prediction}]
